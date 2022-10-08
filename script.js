89e253e56ce917f013f0304495fb5f586e3aa970
Ariel <arielvincennao@hotmail.com>
2022-10-08 16:10:36 -0300

    Added changes in home view, profile view has new format, added loading animation when load or reload main page with progress numbers

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//Workign
+//Working
+
+let body = document.querySelector('body');
+let counter = document.querySelector('.count');
+let divSpinner = document.querySelector('.divSpinner');
+let i = 0; 
+
+(function draw() {
+  if(i <= 100) {
+    var r = Math.random();
+    
+    requestAnimationFrame(draw);  
+    counter.innerHTML = Math.round(i) + '%';
+    
+    if(r < 0.9) { 
+      i = i + r;
+    }
+
+  }else{
+    body.removeChild(divSpinner);
+    body.style.overflow = "visible";
+  } 
+})();
+
+
+
