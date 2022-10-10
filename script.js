dc869a8141b4734aea5e86cfd6dc1a4321845fe0
Ariel <arielvincennao@hotmail.com>
2022-10-09 22:09:23 -0300

    changed and finished counter code completely. Now can setup time and target number

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
 //Working
 
+
 let body = document.querySelector('body');
-let counter = document.querySelector('.count');
+let counterNumber = document.querySelector('.count');
 let divSpinner = document.querySelector('.divSpinner');
-let i = 0; 
+let done = false;
+let time = 1; //In seconds
+let count = 0; //Start number
+let target = 100; //Last number
+let firstInterval = setInterval(counter, time*1000 / target);
 
-(function draw() {
-  if(i <= 100) {
-    var r = Math.random();
-    
-    requestAnimationFrame(draw);  
-    counter.innerHTML = Math.round(i) + '%';
-    
-    if(r < 0.9) { 
-      i = i + r;
-    }
+function counter(){
+  if(count > target){
+      clearInterval(firstInterval);
+  }
+  count++;
+  counterNumber.innerHTML = count;
+}
 
-  }else{
+setInterval(() => {
+  if(done == false){
     body.removeChild(divSpinner);
     body.style.overflow = "visible";
-  } 
-})();
+    done = true;
+  }
+}, time*1000);
+
+
 
 
 
