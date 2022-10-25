863539f767e5c454fb94131c197108c0bbc7e8bf
Ariel <arielvincennao@hotmail.com>
2022-10-25 00:49:06 -0300

    Finished home view carousel, added div with href to gamerelease and moved css inline styles to css file

--- Entregable N2/script.js
+++ Entregable N2/script.js
@@ -45,23 +45,35 @@ function suscribedAlert(){
 
 let prev = document.querySelector("#prev");
 let prev2 = document.querySelector("#prev2");
+let prev3 = document.querySelector("#prev3");
+
 let next = document.querySelector("#next");
 let next2 = document.querySelector("#next2");
+let next3 = document.querySelector("#next3");
+
 let carousel = document.querySelector("#carousel-container");
 let carousel2 = document.querySelector("#carousel-container2");
+let carousel3 = document.querySelector("#carousel-container3");
+
 let track = document.querySelector("#track");
 let track2 = document.querySelector("#track2");
+let track3 = document.querySelector("#track3");
+
 let width = carousel.offsetWidth;
 let width2 = carousel2.offsetWidth;
+let width3 = carousel3.offsetWidth;
+
 let index = 0;
 let index2 = 0;
+let index3 = 0;
 
 window.addEventListener("resize", function () {
   width = carousel.offsetWidth;
   width2 = carousel2.offsetWidth;
+  width3 = carousel3.offsetWidth;
 });
 
-
+//First carousel
 
 next.addEventListener("click", function (e) {
   e.preventDefault();
@@ -85,13 +97,14 @@ prev.addEventListener("click", function () {
 track.offsetWidth - index * width < index * width
 
 
+//Second carousel
 
 next2.addEventListener("click", function (e) {
   e.preventDefault();
   index2 = index2 + 1;
   prev2.classList.add("show");
   track2.style.transform = "translateX(" + index2 * (-width2/4) + "px)";
-  if ((track2.offsetWidth - index2 * (width2/4)) < (index * (width2/8))) {
+  if ((track2.offsetWidth - index2 * (width2/4)) < (index2 * (width2/8))) {
     next2.classList.add("hide");
   }
 });
@@ -106,3 +119,27 @@ prev2.addEventListener("click", function () {
 });
 
 track2.offsetWidth - index2 * width2 < index2 * width2
+
+
+//Third carousel
+
+next3.addEventListener("click", function (e) {
+  e.preventDefault();
+  index3 = index3 + 1;
+  prev3.classList.add("show");
+  track3.style.transform = "translateX(" + index3 * (-width3/4) + "px)";
+  if ((track3.offsetWidth - index3 * (width3/4)) < (index3 * (width3/8))) {
+    next3.classList.add("hide");
+  }
+});
+
+prev3.addEventListener("click", function () {
+  index3 = index3 - 1;
+  next3.classList.remove("hide");
+  if (index3 === 0) {
+    prev3.classList.remove("show");
+  }
+  track3.style.transform = "translateX(" + index3 * (-width3/4) + "px)";
+});
+
+track3.offsetWidth - index3 * width3 < index3 * width3
