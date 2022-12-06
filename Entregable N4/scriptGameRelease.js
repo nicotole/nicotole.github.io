b2a7c28be28bc131f9deb090bec0166747319581
Ariel <arielvincennao@hotmail.com>
2022-12-06 00:03:14 -0300

    Added animations in characters section

--- Entregable N4/scriptGameRelease.js
+++ Entregable N4/scriptGameRelease.js
@@ -48,15 +48,16 @@ function showNext(n) {
 
 
 let title = document.querySelector('.gameReleaseH1');
+let test = document.querySelector('.charactersContainer');
 
 window.onscroll = function() {myFunction()};
 
 function myFunction() {
-  if (document.documentElement.scrollTop > 80) {
+  if (document.documentElement.scrollTop > 1) {
     title.classList.add("hide-title");
-  }else{
+  }else if (document.documentElement.scrollTop <= 0){
     title.classList.remove("hide-title");
-  } 
+  }
 }
 
 // ------------------------------- ej 6
@@ -77,5 +78,34 @@ const observer = new IntersectionObserver(function(entries, observer){
 }, options);
 
 observer.observe(sectionOne);
+
+
+//-------------------- ej 9
+
+
+let sectionChar = document.querySelector(".character1");
+let sectionChar2 = document.querySelector(".character2");
+let sectionChar3 = document.querySelector(".character3");
+
+
+const options1 = {};
+
+
+const observer1 = new IntersectionObserver(function(entries, observer){
+  entries.forEach(entry => {
+    console.log(entry);
+    if (entry.isIntersecting){
+      sectionChar.classList.add("gameReleaseCharacterAction");
+      sectionChar2.classList.add("gameReleaseCharacterAction")
+      sectionChar3.classList.add("gameReleaseCharacterAction")
+    }else{
+      sectionChar.classList.remove("gameReleaseCharacterAction");
+      sectionChar2.classList.remove("gameReleaseCharacterAction")
+      sectionChar3.classList.remove("gameReleaseCharacterAction")
+    }
+  });
+}, options);
+
+observer1.observe(sectionChar);
 
 
